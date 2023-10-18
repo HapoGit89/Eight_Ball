80c2ef58a9bae392afeea23225ab625f61cc56a9
Hannes Porombka <hannes.porombka@gmail.com>
2023-10-18 15:10:07 +0200

    refactored state to app top level

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 import './Eightball.css'
 import Eightball from './Eightball';
 const array = require ('./array')
 
+
 function App() {
+  const [status, setStatus] = useState(true);  // true if eightball is in question state
+  const [object, setObject] = useState({msg:"Think of a question", color: "black"});
+  function handleClick(){
+    console.log("click")
+    if (status===false){
+      setObject({msg:"Think of a question", color: "black"})
+      setStatus(true)
+    }
+    else {
+      const randArrayInd = Math.floor(Math.random()*(array.length-1))
+      setObject(array[randArrayInd])
+      setStatus(false)
+    }
+  }
+  
   return (
-    <div className="App">
-      <form>
-        <button className="button">Restart</button>
-        </form>
-      <Eightball array={array}/> 
+    <div className="App" onClick={handleClick}>
+      <Eightball object={object} /> 
     </div>
   );
 }
--- src/Eightball.js
+++ src/Eightball.js
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
 
-function getObject(array){
-const randNum = Math.floor(Math.random()*(array.length-1))
-return array[randNum]
-}
-
-function Eightball({array}){
-    const [object, setObject] = useState({msg:"Think of a question", color: "black"});
+function Eightball({object}){
+    
 return (
-    <div onClick={()=>setObject(getObject(array))} className="eight-ball" style={{backgroundColor: object.color}}>
-       <h1 class="text">{object.msg}</h1>
+    <div className="eight-ball" style={{backgroundColor: object.color}}>
+       <h1 className="text">{object.msg}</h1>
     </div>
 )
 
