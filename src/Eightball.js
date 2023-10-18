2ca203e3ca026b2ea4d1a53eb12b8f0ab160103f
Hannes Porombka <hannes.porombka@gmail.com>
2023-10-18 13:46:20 +0200

    refactored Eightball.js

--- src/Eightball.js
+++ src/Eightball.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 
+function getObject(array){
+const randNum = Math.floor(Math.random()*(array.length-1))
+return array[randNum]
+}
+
 function Eightball({array}){
     const [object, setObject] = useState({msg:"Think of a question", color: "black"});
 return (
-    <div onClick={()=>setObject(array[Math.floor(Math.random()*(array.length-1))])} className="eight-ball" style={{backgroundColor: object.color}}>
+    <div onClick={()=>setObject(getObject(array))} className="eight-ball" style={{backgroundColor: object.color}}>
        <h1 class="text">{object.msg}</h1>
     </div>
 )
